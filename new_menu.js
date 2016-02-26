19e01de0f3117d1e08634fa9ffe2140a49233e1d
nodes11 <desmarks11@gmail.com>
2016-02-25 16:03:39 -0800

    Added JQuery to add in a dropdown menu feature. Will change the dropdown to a popup later on today.

---
+++ new_menu.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  $("#item_type1").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+
+  $("#item_type2").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+
+  $("#item_type3").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type4").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type5").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type6").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type7").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type8").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type9").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type10").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+  $("#item_type11").click(function (e) {
+    e.stopPropagation();
+    jQuery(this).siblings('#menu_item').toggle();
+  });
+});
