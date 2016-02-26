9484615ed352b05990a45c2efb02c7b850830d91
nodes11 <desmarks11@gmail.com>
2016-02-25 23:44:13 -0800

    A few stylistic things

--- new_menu.js
+++ new_menu.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   $("#item_type1").click(function (e) {
     e.stopPropagation();
 
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[0] == 0){
       overlay.style.display = "block";
       on[0] = 1;
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   $("#item_type2").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[1] == 0){
       overlay.style.display = "block";
       on[1] = 1;
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   $("#item_type3").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[2] == 0){
       overlay.style.display = "block";
       on[2] = 1;
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type4").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[3] == 0){
       overlay.style.display = "block";
       on[3] = 1;
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type5").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[4] == 0){
       overlay.style.display = "block";
       on[4] = 1;
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type6").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[5] == 0){
       overlay.style.display = "block";
       on[5] = 1;
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type7").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[6] == 0){
       overlay.style.display = "block";
       on[6] = 1;
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type8").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[7] == 0){
       overlay.style.display = "block";
       on[7] = 1;
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type9").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[8] == 0){
       overlay.style.display = "block";
       on[8] = 1;
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type10").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[9] == 0){
       overlay.style.display = "block";
       on[9] = 1;
@@ -129,7 +129,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   $("#item_type11").click(function (e) {
     e.stopPropagation();
-    $("#popup").html('<h2 id="close" class="close">X</h2>' + jQuery(this).siblings('#menu_item').text());
+    $("#popup").html('<h2 id="close" class="close">X</h2>' + $(this).siblings('#menu_item').text());
     if (on[10] == 0){
       overlay.style.display = "block";
       on[10] = 1;
