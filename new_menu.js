6f9fbed7de2426948be2505c72408a41a1ce32a2
nodes11 <desmarks11@gmail.com>
2016-02-28 22:28:49 -0800

    The popup is now fixed position. The background dims when the popup displays. And you click in the dimmed area to close the popup

--- new_menu.js
+++ new_menu.js
@@ -3,22 +3,27 @@ var on = 0;
 
 document.addEventListener("DOMContentLoaded", function (event) {
   var overlay = document.getElementById("popup");
+  var background = document.getElementById("dim");
+
 
   $(".it-outer").click(function (e) {
     e.stopPropagation();
     $(".popup").html($(this).children('.section').html());
     if (on == 0){
+      background.style.display = "block";
       overlay.style.display = "block";
       on = 1;
     }
     else {
+      background.style.display = "none";
       overlay.style.display = "none";
       on = 0;
     }
   });
 
-  $("#popup").click(function (e) {
+  $("#dim").click(function (e) {
     on = 0;
+    $("#dim").css("display", "none");
     $("#popup").css("display", "none");
   });
 
